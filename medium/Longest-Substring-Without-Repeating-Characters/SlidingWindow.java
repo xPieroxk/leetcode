940dfd6d6ff908dcab2f162ed0bcef722fecc19a
Piero <piero.hierro1@gmail.com>
2022-08-01 11:23:42 +0200

    Update SlidingWindow.java

--- medium/Longest-Substring-Without-Repeating-Characters/SlidingWindow.java
+++ medium/Longest-Substring-Without-Repeating-Characters/SlidingWindow.java
@@ -1,5 +1,24 @@
-class SlidingWindow {
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
+class Solution {
+    
+    // O(n) time and O(1) space
     public int lengthOfLongestSubstring(String s) {
-        // implement sliding windows technique
+        var characters = new HashMap<Character, Integer>();
+        int left = 0, right = 0, maxLen = 0;
+        while (right < s.length()) {
+            var c = s.charAt(right);
+            if (characters.containsKey(c)) {
+                // remove all characters from the left to the repeated character
+                IntStream.range(left, characters.get(c)).forEach(i -> characters.remove(s.charAt(i)));
+                // update the left pointer
+                left = characters.get(c) + 1;
+            }
+            characters.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
     }
 }
