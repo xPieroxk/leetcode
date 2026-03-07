9b020540a14838f853be0b192dc2a1d0e49c8893
pierohierro <piero.hierro1@gmail.com>
2026-03-07 20:36:57 +0100

    75 alternative solution with one pass, complexity remains the same though

--- medium/75/solution.py
+++ medium/75/solution.py
@@ -12,3 +12,18 @@ class Solution(object):
                 nums[curr] = i
                 counts[i] -= 1
                 curr += 1
+
+'''    class Solution(object):
+        def sortColors(self, nums):
+            l = m = 0
+            r = len(nums) - 1
+            while m <= r:
+                if nums[m] == 0:
+                    nums[l], nums[m] = nums[m], nums[l]
+                    m += 1
+                    l += 1
+                elif nums[m] == 1:
+                    m += 1
+                else:
+                    nums[m], nums[r] = nums[r], nums[m]
+                    r -= 1'''
