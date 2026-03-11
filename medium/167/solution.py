b7a3092fe54e5dee4a1601a3f6bb3e6febc30969
pierohierro <piero.hierro1@gmail.com>
2026-03-12 00:28:24 +0100

    167

---
+++ medium/167/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l=0
+        r= len(numbers) -1
+
+        while True:
+            sum = numbers[l] + numbers[r]
+            if sum>target:
+                r-=1
+            elif sum<target:
+                l+=1
+            else:
+                return [l+1,r+1]
