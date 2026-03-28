3fb0e65d9897d8c90ad58cb1c0a698bf0aeff348
pierohierro <piero.hierro1@gmail.com>
2026-03-28 20:28:23 +0100

    155

---
+++ medium/155/solution.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.mins or val <= self.mins[-1]:
+            self.mins.append(val)
+
+    def pop(self) -> None:
+        to_pop = self.stack.pop()
+        if to_pop == self.mins[-1]:
+            self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
