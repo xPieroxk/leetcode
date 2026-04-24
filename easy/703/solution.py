a0d80e9eaf9372efb22454950b9fb57145890859
pierohierro <piero.hierro1@gmail.com>
2026-04-24 21:57:40 +0200

    703

---
+++ easy/703/solution.py
@@ -0,0 +1,18 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap= nums
+        heapq.heapify(self.min_heap)
+        for i in range(len(nums)-k):
+            heapq.heappop(self.min_heap)
+
+
+    def add(self, val: int) -> int:
+        if len(self.min_heap)<self.k:
+            heapq.heappush(self.min_heap,val)
+        elif val > self.min_heap[0]:
+            heapq.heappushpop(self.min_heap, val)
+        return self.min_heap[0]
