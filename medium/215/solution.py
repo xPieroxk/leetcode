7923a7e6801640fbb184e7937ac39aea5ae1316f
pierohierro <piero.hierro1@gmail.com>
2026-04-26 18:30:40 +0200

    215 Quickselect + Dutch National Flag algorithm to avoid all duplicates test case of leetcode

--- medium/215/solution.py
+++ medium/215/solution.py
@@ -36,4 +36,32 @@ class Solution:
             else:
                 return nums[p]
 
-#
+# Quickselect + Dutch National Flag algorithm to avoid all duplicates test case of leetcode
+import random
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        kth_index = len(nums) - k
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            pivot = nums[random.randint(l, r)]
+            lt, i, gt = l, l, r
+
+            while i <= gt:
+                if nums[i] < pivot:
+                    nums[i], nums[lt] = nums[lt], nums[i]
+                    lt += 1
+                    i += 1
+                elif nums[i] > pivot:
+                    nums[i], nums[gt] = nums[gt], nums[i]
+                    gt -= 1
+                else:
+                    i += 1
+
+            if kth_index < lt:
+                r = lt - 1
+            elif kth_index > gt:
+                l = gt + 1
+            else:
+                return nums[kth_index]
