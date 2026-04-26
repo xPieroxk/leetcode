e6db7b0e9dfba268639a1fe8adb60bce59799a90
pierohierro <piero.hierro1@gmail.com>
2026-04-26 02:16:01 +0200

    215 quickselect solution

--- medium/215/solution.py
+++ medium/215/solution.py
@@ -9,3 +9,31 @@ class Solution:
                 heapq.heappop(h)
 
         return h[0]
+
+# quickselect
+import random
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        kth_idx = len(nums) - k
+        l, r = 0, len(nums) - 1
+        while True:
+            pivot_idx = random.randint(l, r)
+            nums[r], nums[pivot_idx] = nums[pivot_idx], nums[r]
+
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p < kth_idx:
+                l = p + 1
+            elif p > kth_idx:
+                r = p - 1
+            else:
+                return nums[p]
+
+#
