c91fda3214d7b1730c3a9a04fa4d254f5bc1588b
pierohierro <piero.hierro1@gmail.com>
2026-05-09 18:41:04 +0200

    208

---
+++ medium/208/solution.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, val=None, isEnd=False):
+        self.isEnd = isEnd
+        self.children = {}
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node(c)
+            curr = curr.children[c]
+
+        curr.isEnd = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return curr.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return True
