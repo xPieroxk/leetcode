03d22230c43cfe21516e97affaa94c4f0130c588
pierohierro <piero.hierro1@gmail.com>
2026-05-23 23:40:12 +0200

    70

--- easy/70/solution.py
+++ easy/70/solution.py
@@ -10,3 +10,19 @@ class Solution(object):
         for i in range(3,n+1):
             result[i] = result[i-1] + result[i-2]
         return result[n]
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        if n == 2: return 2
+
+        prev = 1
+        prev_prev = 2
+
+        for i in range(2, n):
+            tmp = prev_prev
+            prev_prev = prev + prev_prev
+            prev = tmp
+
+        return prev_prev
