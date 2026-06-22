79a4cd052134a19353cfb111467d49b0d39ba58e
pierohierro <piero.hierro1@gmail.com>
2026-06-23 00:08:42 +0200

    55

--- medium/55/solution.py
+++ medium/55/solution.py
@@ -11,3 +11,17 @@ class Solution(object):
             jumps = max(jumps,n)
             jumps -=1
         return True
+
+
+# moving forward
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        reach = 0
+
+        for i in range(n):
+            if i > reach: return False
+            reach = max(reach, i + nums[i])
+            if reach >= n - 1: return True # optimization, not needed
+
+        return reach >= n - 1
