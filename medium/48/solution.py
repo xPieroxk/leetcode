93a052b69af58dbe390e0b03c725a52dd7bb992f
pierohierro <piero.hierro1@gmail.com>
2026-08-06 23:31:14 +0200

    48

---
+++ medium/48/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        # reverse horizontally
+        for i in range(n // 2):
+            matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]
+
+        # transpose
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
